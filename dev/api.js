a539888abc8f5195bb9ccd95a7f4ec3fc4c375d2
BELLO ABDULMATEEN <abdulmateenbinbello@gmail.com>
2020-04-04 12:14:15 +0100

    Endpoints created, implementation next

--- dev/api.js
+++ dev/api.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('API Started, and running successfully');
 })
+
+app.get('/blockchain', function (req, res) {
+    // This will return the entire blockchain for us!!!
+});
+
+app.post('/transaction', function (req, res) {
+    // This will be used to create new transactions into the blockchain
+});
+
+app.get('/mine', function (req, res) {
+    // This will be used to mine new blocks into the blockchain
+});
  
-app.listen(3000)
+app.listen(3000, function () {
+    console.log('Started and Listening on http://localhost:3000');
+})
